ec3bb705051deed3b4e414dc923ac110ce758f7e
douglas <douglas.webster2107@gmail.com>
2023-07-19 09:12:05 +0100

    Commenced scaffolding make_json_library code

---
+++ include/soko_cpp_tools/make_json_library.hpp
@@ -0,0 +1,6 @@
+#ifndef MAKE_JSON_LIBRARY_HPP
+#define MAKE_JSON_LIBRARY_HPP
+
+#include <soko_cpp_tools/make_json_library_export.hpp>
+
+#endif
---
+++ src/make_json_library/make_json_library.cpp
@@ -0,0 +1,36 @@
+#include <format>
+#include <string>
+#include <iostream>
+
+#include <optional>
+
+#include <CLI/CLI.hpp>
+#include <internal_use_only/config.hpp>
+#include <spdlog/spdlog.h>
+
+
+int main(int argc, const char **argv)
+{
+  try {
+    CLI::App app{ std::format(
+      "{} version {}", soko_cpp_tools::cmake::project_name, soko_cpp_tools::cmake::project_version) };
+
+    std::optional<std::string> message;
+    app.add_option("-m,--message", message, "A message to print back out");
+    bool show_version = false;
+    app.add_flag("--version", show_version, "Show version information");
+
+    CLI11_PARSE(app, argc, argv);
+
+    if (show_version) {
+      std::cout << std::format("{}\n", soko_cpp_tools::cmake::project_version);
+      return EXIT_SUCCESS;
+    }
+
+  } catch (const std::exception &e) {
+    spdlog::error("Unhandled exception in main: {}", e.what());
+  }
+  
+  fmt::print("Starting codeing Make Json!");
+  return 0;
+}
